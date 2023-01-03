047c8df85da37053a3e20e335d6fc0379a330ea8
FreytagDom <dominicfreytag@web.de>
2023-01-03 20:43:04 +0100

    add title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,39 @@
 import { GlobalStyle } from '../components/GlobalStyle';
 import Layout from '../components/Layout';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session} basePath="/api/auth">
-      <GlobalStyle />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
+    <>
+      <Head>
+        <title>Insulin App</title>
+        <meta name="description" content="Insulin app" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/leereSpritze.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/leereSpritze.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/leereSpritze.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <SessionProvider session={session} basePath="/api/auth">
+        <GlobalStyle />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
+    </>
   );
 }
 
