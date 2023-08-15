23634c38369c76689e80c7c4c5cdd4aaee583a80
FreytagDom <dominicfreytag@web.de>
2023-08-15 09:31:01 +0200

    delete unused component

--- components/getUserMail.js
+++
@@ -1,8 +0,0 @@
-import { useSession } from "next-auth/react";
-
-
-export default function UserMail() {
-const {data: session} = useSession();
-const userMail = session.user.email;
-return userMail;
-}
